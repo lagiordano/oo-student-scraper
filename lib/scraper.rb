83668087e832a5f658a86dc1f1eae3e23559b60e
Lauren Giordano <laurengiordano94@gmail.com>
2023-05-18 10:21:07 +1000

    commit tree a6cb281fec97b61e8896a0ce6bd4e7ee04d7e928

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,19 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    doc = Nokogiri::HTML(open(index_url))
+    students = doc.css(".student-card").map do |student| 
+      {
+        :name => student.css("h4.student-name").text,
+        :location => student.css("p.student-location").text,
+        :profile_url => student.css("a")[0].attributes['href'].value
+      }
     
+    end
   end
 
   def self.scrape_profile_page(profile_url)
@@ -13,3 +22,4 @@ class Scraper
 
 end
 
+Scraper.scrape_index_page "https://learn-co-curriculum.github.io/student-scraper-test-page/"
